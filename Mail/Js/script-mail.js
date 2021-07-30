b8729a160e3acef51547a813abda027ff5156e14
eliaraguso <elia.raguso@gmail.com>
2021-07-30 17:17:03 +0200

    modificato script mail

--- Mail/Js/script-mail.js
+++ Mail/Js/script-mail.js
@@ -4,16 +4,23 @@ var mailUtente = prompt("Vuoi scoprire se hai accesso al nostro Vip Club? Inseri
 // Definisco il gruppo di mail selezionate per l'accesso esclusivo al Vip Club.
 var verifiedMails = ["ginoamiocuggino@mail.com", "pincopallo@mail.com", "mail@mail.com"];
 
+// Definisco una variabile di controllo
+var accesso = false
+
 // Controllo che la mail inserita dall'utente sia nella lista di chi può accedere
 for ( var i = 0; i < verifiedMails.length; i++) {
-    // Se fa parte della lista scrivo questo    
+    // Se fa parte della lista la variabile di controllo assume il valore di true 
     if (mailUtente == verifiedMails[i]) {
-        alert("Complimenti, abbiamo verificato la tua mail e d'ora in poi sei ufficialmente dei nostri.");
-    //   Se invece non fa parte della lista scrivo questo.s  
-    } else {
-        alert("Ci dispiace ma non potrai mai far parte del nostro gruppo esclusivo.");
+        accesso = true;
     }
 }
 
 
-  
+    // Se la variabile assume il valore di true e quindi la mail inserita fa parte della lista scrivo
+if (accesso = false){
+    alert("Ci dispiace ma non potrai mai far parte del nostro gruppo esclusivo.");  
+
+    // Se la variabile assume il valore di true e quindi la mail inserita fa parte della lista scrivo
+} else {
+    alert("Complimenti, abbiamo verificato la tua mail e d'ora in poi sei ufficialmente dei nostri.");
+}
