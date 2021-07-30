870b642f681f830c9bc214eaaa1664bbd1c3af0c
eliaraguso <elia.raguso@gmail.com>
2021-07-30 15:03:07 +0200

    corretto script

--- Mail/Js/script-mail.js
+++ Mail/Js/script-mail.js
@@ -9,7 +9,7 @@ if (mailUtente == verifiedMails[0]) {
     alert("Complimenti, abbiamo verificato la tua mail e d'ora in poi sei ufficialmente dei nostri. Il tuo numero utente è 1");
 } else if (mailUtente == verifiedMails[1]) {
     alert("Complimenti, abbiamo verificato la tua mail e d'ora in poi sei ufficialmente dei nostri. Il tuo numero utente è 2");
-} else if (mailUtente == verifiedMails[1]) {
+} else if (mailUtente == verifiedMails[2]) {
     alert("Complimenti, abbiamo verificato la tua mail e d'ora in poi sei ufficialmente dei nostri. Il tuo numero utente è 3");
 } else {
     alert("Ci dispiace ma non potrai mai entrare a far parte del nostro Vip Club");
